166ba2f38649935f9bdd6dbb3570726982d76e4a
Karan <karan.209302368@muj.manipal.edu>
2024-02-26 01:47:50 +0530

    Changes made to Que12

--- eclipse-workspace/HughesJavaExercises/src/com/hughes/exercises/Que12.java
+++ eclipse-workspace/HughesJavaExercises/src/com/hughes/exercises/Que12.java
@@ -1,10 +1,104 @@
 package com.hughes.exercises;
-
+/**
+ * @author Karan
+ *
+ */
 public class Que12 {
+	/*
+	 CoreJava_Assessment1(Creating the class structure)
+	 */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+	}
+	
+	public static class Account{
+		int accountId;
+		String accountName;
+		String Address;
+		long depositAmount;
+		
+		public void getDetails() {
+			
+		}
+		
+		public void showDetails() {
+			
+		}
+		
+	}
+	
+	public static class Loan extends Account{
+		int loanId;
+		String loanType;
+		long loanAmmount;
+		
+		public void getLoan() {
+			
+		}
+		
+		public void showLoanDetails() {
+			
+		}
+		
+	}
+	
+	public static class Transaction extends Loan{
+		long Amount;
+		
+		public void depositAmount() {
+			
+		}
+		
+		public void withdrawAmount() {
+			
+		}
+		
+		public void payLoan() {
+			
+		}
+		
+		public void showAccountDetails() {
+			
+		}
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
